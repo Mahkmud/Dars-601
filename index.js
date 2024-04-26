f1f451ac2de258d9b02df591d4d9dda3e3e11bc6
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-04-26 09:32:41 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,9 @@
+let son1 = Number(prompt("Birinchi sonni kiriting "));
+let son2 = Number(prompt("Ikkinchi sonni kiriting "));
+let amal = String(prompt("Amalni kiriting  (+,-,*,/)"));
+
+
+if (amal === "+"){
+let yigindi =son1=son2
+alert(son1 + "+" + son + "=" + yigindi)
+}
